47ece1b2bb6ffa33b8e246ea0f85b56d6db26ba3
Likithasri9 <80267437+Likithasri9@users.noreply.github.com>
2021-08-03 08:09:44 +0530

    Add files via upload

---
+++ Rubber.js
@@ -0,0 +1,28 @@
+class Rubber{
+	constructor(x,y,r)
+	{   var options={
+		 	'restitution':0.3,
+		 	'friction':5,
+		 	'density':1
+		 }
+     	this.body=Bodies.rectangle(this.x, this.y, (this.r-20)/2, options)
+		World.add(world, this.body);
+		this.x=x;
+		this.y=y;
+		this.r=r
+
+	}
+	display()
+	{
+			var rubberpos=this.body.position;		
+			push()
+			translate(rubberpos.x, rubberpos.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			stroke("black");
+			fill("darkblue");
+			rect(0,0,this.r, this.r);
+			pop()
+	}
+
+}
